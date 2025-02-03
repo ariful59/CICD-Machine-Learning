c286d284273b427f780eff4c63913111134e4cbc
Md Ariful Amin <arifulamindu@gmail.com>
2025-02-04 00:41:06 +0100

    fix

--- App/drug_app.py
+++ App/drug_app.py
@@ -1,3 +1,4 @@
+
 import gradio as gr
 import skops.io as sio
 import warnings
@@ -59,6 +60,8 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
+article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+
 
 gr.Interface(
     fn=predict_drug,
@@ -67,5 +70,6 @@ gr.Interface(
     examples=examples,
     title=title,
     description=description,
+    article=article,
     theme=gr.themes.Soft(),
 ).launch()
