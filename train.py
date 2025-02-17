b32495e114047b475d94d391b82018688ed70c44
USER_EMAIL <USER_EMAIL>
2025-02-17 18:56:50 +0000

    Update with new results

--- train.py
+++ train.py
@@ -8,27 +8,31 @@ from sklearn.pipeline import Pipeline
 import skops.io as sio
 
 
-data = pd.read_csv('./Data/drug.csv')
+data = pd.read_csv("./Data/drug.csv")
 data = data.sample(frac=1, random_state=42)
 
 from sklearn.model_selection import train_test_split
 
-X = data.drop('Drug', axis=1).values
+X = data.drop("Drug", axis=1).values
 y = data.Drug.values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 
-transformer = ColumnTransformer(transformers=[
-    ('encoder', OrdinalEncoder(), [1,2,3]),
-    ('imputer', SimpleImputer(strategy="median"), [0,4]),
-    ('scaler', StandardScaler(), [0,4])
-])
+transformer = ColumnTransformer(
+    transformers=[
+        ("encoder", OrdinalEncoder(), [1, 2, 3]),
+        ("imputer", SimpleImputer(strategy="median"), [0, 4]),
+        ("scaler", StandardScaler(), [0, 4]),
+    ]
+)
 
 pipe = Pipeline(
-    steps = [
+    steps=[
         ("preprocessor", transformer),
-        ('model', RandomForestClassifier(n_estimators=10, random_state=42))
+        ("model", RandomForestClassifier(n_estimators=10, random_state=42)),
     ]
 )
 
@@ -38,7 +42,7 @@ predictions = pipe.predict(X_test)
 
 accu = accuracy_score(y_test, predictions)
 print(accu)
-f1 = f1_score(y_test, predictions, average='macro')
+f1 = f1_score(y_test, predictions, average="macro")
 print(f"accuracy: {round(accu, 2)} f1: {round(f1, 2)}")
 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
@@ -49,15 +53,12 @@ disp = ConfusionMatrixDisplay(cm, display_labels=pipe.classes_)
 disp.plot()
 plt.savefig("./Results/model_results.png", dpi=120)
 
-with open('./Results/metrics.txt', 'w') as f:
+with open("./Results/metrics.txt", "w") as f:
     f.write(f"accuracy: {round(accu, 2)} f1: {round(f1, 2)}")
 
-sio.dump(pipe, './Model/drug_pipeline.skops')
+sio.dump(pipe, "./Model/drug_pipeline.skops")
 
 import joblib
 
 # Save your trained model
 joblib.dump(pipe, "model.pkl")
-
-
-
